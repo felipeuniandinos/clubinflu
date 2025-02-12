3aa39212e36ea295cc5dbe86c3aa28e22af35971
Esteban Reyes Marcelo <e.reyesm@uniandes.edu.co>
2025-02-12 18:01:21 -0500

    version incial para desarrollo

--- ClubInfluApp/Program.cs
+++ ClubInfluApp/Program.cs
@@ -1,49 +1,11 @@
-using ClubInfluApp.BusinessLogic.Interfaces;
-using ClubInfluApp.BusinessLogic.Services;
-using ClubInfluApp.Data.Interfaces;
-using ClubInfluApp.Data.Services;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Google;
-
 var builder = WebApplication.CreateBuilder(args);
-
-
-// Configurar autenticación con Google
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-})
-.AddCookie(options =>
-{
-    options.LoginPath = "/Account/Login";
-    options.LogoutPath = "/Account/Logout";
-    options.AccessDeniedPath = "/Account/AccessDenied";
-})
-.AddGoogle(googleOptions =>
-{
-    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-    googleOptions.CallbackPath = "/signin-google";
-});
-
-// Configurar autorización con roles
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-    options.AddPolicy("RequireInfluencerRole", policy => policy.RequireRole("Influencer"));
-    options.AddPolicy("RequireProviderRole", policy => policy.RequireRole("Provider"));
-});
-
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//Services
-builder.Services.AddScoped<IUserService, UserService>();
 
-//Repositories
-builder.Services.AddScoped<IUserRepository, UserRepository>();
+//Add repositories to the container.
+
 
 var app = builder.Build();
 
