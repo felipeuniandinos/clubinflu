62699a8d827f8862522cccebc97c4c67499ad9ee
DIGITAL-SOLUTIONS-EREYESM <ereyes@digitalsolutionsteam.com.co>
2025-02-24 18:21:17 -0500

    Crear solicitud usuario empresa (#2)
    * Se agrega los modelos para la solicitud de creacion de usuario empresa, junto con el repositorio de usuario empresa para crear un usuario empresa en la db
    
    * Se hace la version incial del home
    
    * Version funcional de crear un usuario de tipo empresa
    
    * Version funcional de crear un usuario de tipo empresa v2
    
    ---------
    
    Co-authored-by: Esteban Reyes Marcelo <e.reyesm@uniandes.edu.co>

---
+++ ClubInfluApp/Controllers/UsuarioEmpresaController.cs
@@ -0,0 +1,40 @@
+﻿using ClubInfluApp.BusinessLogic.Interfaces;
+using ClubInfluApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace ClubInfluApp.Controllers
+{
+    public class UsuarioEmpresaController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUsuarioEmpresaService _usuarioEmpresaService;
+
+        public UsuarioEmpresaController(ILogger<HomeController> logger, IUsuarioEmpresaService usuarioEmpresaService)
+        {
+            _logger = logger;
+            _usuarioEmpresaService = usuarioEmpresaService;
+        }
+
+        [HttpGet]
+        public IActionResult CrearUsuarioEmpresa()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CrearUsuarioEmpresa(NuevoUsuarioEmpresaViewModel nuevoUsuarioEmpresaViewModel)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(nuevoUsuarioEmpresaViewModel); 
+            }
+
+            int idUsuarioEmpresa = _usuarioEmpresaService.CrearUsuarioEmpresa(nuevoUsuarioEmpresaViewModel);
+
+            ViewBag.Mensaje = "Usuario creado exitosamente. Con el id:" + idUsuarioEmpresa;
+            return View();
+        }
+    }
+}
